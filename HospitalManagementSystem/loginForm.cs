9b47e141654687a0f8963bd70842499243071cf1
AdamBrown04 <adambrown2004@outlook.com>
2024-02-13 09:18:36 +0000

    Add project files.

---
+++ HospitalManagementSystem/loginForm.cs
@@ -0,0 +1,15 @@
+namespace HospitalManagementSystem
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
